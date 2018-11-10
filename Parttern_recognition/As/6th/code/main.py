b1b30d53ca8985c81120b8d35a3877117856f259
rsy56640 <851911672@qq.com>
2018-11-10 11:57:50 +0800

    PR as-6

---
+++ Parttern_recognition/As/6th/code/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+xi = [3.2, 3.6, 3, 6, 2.5, 1.1]
+h = np.arange(1, 4, 0.5)
+VN = 6.4
+
+
+def p6(x: float, hN: float) -> float:
+    return sum(map(norm.pdf, map(lambda _xi: (x - _xi) / hN, xi))) / len(xi) / VN
+
+
+x = np.arange(0, 6, 0.1)
+
+for hN in h:
+    p6_hN = lambda x: p6(x, hN)
+    y = list(map(p6_hN, x))
+    plt.plot(x, y)
+
+plt.legend(list(map(lambda hN: "hN = " + str(hN), h)), loc='upper left')
+plt.xlabel("x")
+plt.ylabel("probability")
+
+plt.show()
